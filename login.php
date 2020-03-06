39c8f2a617e564844ec6d000987ae872089aed24
Azimkhon Khudayberdiev <azeemborz@gmail.com>
2020-03-06 16:58:15 +0500

    link

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 
 <body>
 	<h2>Login Page</h2>
-	<a href=index.php>Click here to go back<br/><br/>
+	<a href=index.php> Click here to go back<br/><br/>
 	<form action=”checklogin.php” method=”POST”>
 	Enter Username: 
 	<input type=”text” name=”username” required=”required” />
