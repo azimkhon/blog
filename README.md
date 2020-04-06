02b4e6aedce193df785a8e787d318cd88fafe913
Azimkhon Khudayberdiev <azeemborz@gmail.com>
2020-04-07 04:18:20 +0500

    adding styles and view

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# blog
+# simple blog with basic CRUD admin panel
--- index.php
+++ index.php
@@ -1,30 +1,52 @@
-<?php session_start(); ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
-    <meta charset="utf-8">
-    <title>Welcome, <?=$_SESSION['name']?> </title>
-
+    <meta charset="UTF-8">
+    <title>Personal Blog</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://kit.fontawesome.com/c221214394.js" crossorigin="anonymous"></script>
 </head>
-
 <body>
+    <header>
+    	<div id="logo">
+    		<a href="/blog" title="Main page">Personal Blog</a> 
+    	</div>
 
-    <?php
-    
-    if($_SESSION['reg_success']) {
-        echo '<p> ' . $_SESSION['reg_success'] . ' </p>';
-    }    
+    	<div id="menuhead">
+    		<div>
+    			<a href="about.php">About us</a>
+    		</div> 
+    		<div>
+    			<a href="contact.php">Contact us</a>
+    		</div>
+    	</div>
 
-    unset($_SESSION['reg_success']);
-   
-    ?>
+    	<div id="regauth">
+    		<a href="auth.php"> Sign in </a> | <a href="reg.php">Sign up</a>
+    	</div>
+    </header>
 
-    <a href=login.php> Click here to login </a>
-         <br> <br>
-    <a href=register.php> Click here to register </a>
+    <div id="wrapper">
+
+    	<div id="leftcol">
+    		ss
+    	</div>
+
+    	<div id="rightcol">
+    		ss
+    	</div>
+
+    </div>
+
+
+    <footer>
+    	<div id="socials">
+    		<a href="#" title="Twitter page"><i class="fab fa-twitter-square fa-2x"></i></a>
+    		<a href="#" title="Facebook community"><i class="fab fa-facebook-square fa-2x"></i></a>
+    	</div>
+    	<div id="rights">
+    		All rights are reserved &copy; <?php echo date('Y') ?>
+    	</div>
+    </footer>
 </body>
-
 </html>
