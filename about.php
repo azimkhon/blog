74e8f6282abedef53b3cd967c888228b5b5a89ef
Azimkhon Khudayberdiev <azeemborz@gmail.com>
2020-06-10 22:02:56 +0500

    check

--- about.php
+++ about.php
@@ -14,6 +14,8 @@ require_once "blocks/head.php"?>
 <div id="wrapper">
     <div id="leftcol">
             <div class="about-us">
+
+            	Checking for commit
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis dolorem ducimus fugiat officia perspiciatis. Alias culpa, deleniti earum error facilis illo illum itaque perferendis reprehenderit repudiandae saepe sequi soluta temporibus?
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias autem et exercitationem minus, non officiis quas veniam vero. Aut blanditiis cumque dolore explicabo facilis iure, minima optio sit tempore ullam?
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, accusantium ad alias atque cupiditate ea eligendi ipsum, laboriosam magnam molestiae molestias mollitia nesciunt, quidem quis ratione ullam vero? Minima, natus.
